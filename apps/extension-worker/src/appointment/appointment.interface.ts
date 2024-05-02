7c5726ec5ecc20741ce68d9175be8c92464bd527
charlinto <fyaklekpanshak@gmail.com>
2024-05-02 14:59:11 +0100

    auth modify

--- apps/extension-worker/src/appointment/appointment.interface.ts
+++ apps/extension-worker/src/appointment/appointment.interface.ts
@@ -1,11 +1,15 @@
+import { CreateAppointmentDto } from './dto/dto';
+import { FindDto } from './dto/find_dto';
+import { UpdateDto } from './dto/update_dto';
+
 export interface Appointment {
-  ScheduleAppointment(data): Promise<Appointment>;
-  GetVisits(data): Promise<Appointment>;
-  AddVisit(data): Promise<Appointment>;
-  RemoveVisit(data): Promise<Appointment>;
-  findByid(data): Promise<Appointment>;
-  findBytime(data): Promise<Appointment>;
-  findBystatus(data): Promise<Appointment>;
-  findByworkerProfileId(data): Promise<Appointment>;
-  UpdateProperty(data: Promise<Appointment>);
+  ScheduleAppointment(data: CreateAppointmentDto): Promise<Appointment>;
+  GetVisits(data: FindDto): Promise<Appointment>;
+  AddVisit(data: UpdateDto): Promise<Appointment>;
+  RemoveVisit(data: UpdateDto): Promise<Appointment>;
+  findByid(data: FindDto): Promise<Appointment>;
+  findBytime(data: FindDto): Promise<Appointment>;
+  findBystatus(data: FindDto): Promise<Appointment>;
+  findByworkerProfileId(data: FindDto): Promise<Appointment>;
+  UpdateProperty(data: UpdateDto): Promise<Appointment>;
 }
